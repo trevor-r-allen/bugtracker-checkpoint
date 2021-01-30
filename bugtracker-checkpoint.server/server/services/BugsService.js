8a2a4c743b506fa3a90a8e6717a941e2d4bdfa39
trevor-r-allen <tallen2699@gmail.com>
2021-01-30 16:49:03 -0700

    date fix

--- bugtracker-checkpoint.server/server/services/BugsService.js
+++ bugtracker-checkpoint.server/server/services/BugsService.js
@@ -36,7 +36,7 @@ class BugsService {
     const date = new Date()
     const bug = await dbContext.Bugs.findById({ _id: id, creatorId: userId })
     bug.closed = true
-    bug.closedDate = date.toLocaleDateString() + date.toLocaleTimeString()
+    bug.closedDate = date.toLocaleDateString() + ' ' + date.toLocaleTimeString()
     const closed = await dbContext.Bugs.findOneAndUpdate({ _id: id, creatorId: userId }, bug, { new: true }).populate('creator')
     if (!closed) {
       throw new BadRequest('You are not the creator or this bug does not exist')
