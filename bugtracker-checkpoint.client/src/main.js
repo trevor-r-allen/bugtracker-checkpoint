c7ed83444ef115623ce430574f567a26d3d61642
trevor-r-allen <tallen2699@gmail.com>
2021-01-31 12:54:50 -0700

    imported sweetalerts for use

--- bugtracker-checkpoint.client/src/main.js
+++ bugtracker-checkpoint.client/src/main.js
@@ -6,10 +6,12 @@ import router from './router'
 import bootstrap from 'bootstrap'
 import jquery from 'jquery'
 import popper from 'popper.js'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 const root = createApp(App)
 registerGlobalComponents(root)
 
 root
-  .use(router, bootstrap, jquery, popper)
+  .use(router, bootstrap, jquery, popper, VueSweetalert2)
   .mount('#app')
