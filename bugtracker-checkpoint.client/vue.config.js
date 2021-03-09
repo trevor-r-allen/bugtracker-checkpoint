312cabc26e90904731ce5bac5823cb9a04489d23
trevor-r-allen <tallen2699@gmail.com>
2021-03-09 13:14:01 -0700

    heroku fix

--- bugtracker-checkpoint.client/vue.config.js
+++ bugtracker-checkpoint.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
- // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
+  // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
-  outputDir: 'docs',
+  outputDir: '../server/client',
   productionSourceMap: false
 }
